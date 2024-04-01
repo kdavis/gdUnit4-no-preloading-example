4ba41109561659a7a954e58988b41d3f4ce763a4
Keir <keir@sneezey.uk>
2024-04-01 13:58:27 +0100

    fix: make more aligned

--- src/audio.gd
+++ src/audio.gd
@@ -1,11 +1,26 @@
 extends Node
 
-var is_working = false
+var music_stream = null
+var is_ambient_playing = false
+var bus_ambient_name = "Ambient"
 
 func _ready():
-	var music = load("res://AudioCoffee - Warm Autumn Day.mp3")
+	_load_music()
+	play_ambient_music()
+
+func _load_music():
+	music_stream = load("res://AudioCoffee - Warm Autumn Day.mp3")
+
+func _setup_audio_stream_player(bus):
 	var audio_stream_player = AudioStreamPlayer.new()
+	audio_stream_player.bus = bus
 	self.add_child(audio_stream_player)
-	audio_stream_player.stream = music
-	audio_stream_player.play()
-	is_working = true
+	return audio_stream_player
+
+func play_ambient_music():
+	if not is_ambient_playing and music_stream:
+		var player = _setup_audio_stream_player(bus_ambient_name)
+		player.stream = music_stream
+		player.play()
+
+		is_ambient_playing = true
