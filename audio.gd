c4c72da4e500f13a09753bd4c61eed8ee585e0ad
Keir <keir@sneezey.uk>
2024-04-01 13:25:22 +0100

    feat: add to actually playing

--- audio.gd
+++ audio.gd
@@ -3,5 +3,9 @@ extends Node
 var is_working = false
 
 func _ready():
-	var player = load("res://AudioCoffee - Warm Autumn Day.mp3")
+	var music = load("res://AudioCoffee - Warm Autumn Day.mp3")
+	var audio_stream_player = AudioStreamPlayer.new()
+	self.add_child(audio_stream_player)
+	audio_stream_player.stream = music
+	audio_stream_player.play()
 	is_working = true
