87b30435e2e47067ccdda1b6398b2eeaa0d1e88e
Keir <keir@sneezey.uk>
2024-04-01 14:36:00 +0100

    fix: remove bus

--- src/audio.gd
+++ src/audio.gd
@@ -2,12 +2,10 @@ extends Node
 
 var music_stream = null
 var is_ambient_playing = false
-var bus_ambient_name = "Ambient"
 
 func _ready():
 	music_stream = load("res://AudioCoffee - Warm Autumn Day.mp3")
 	var audio_stream_player = AudioStreamPlayer.new()
-	audio_stream_player.bus = bus_ambient_name
 	self.add_child(audio_stream_player)
 
 	if not is_ambient_playing and music_stream:
