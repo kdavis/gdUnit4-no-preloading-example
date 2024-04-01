45264fe1f26b70523f4ef39ec65d2af4b28d046f
Keir <keir@sneezey.uk>
2024-04-01 13:38:06 +0100

    feat: use before_test

--- src/test/audio_test.gd
+++ src/test/audio_test.gd
@@ -4,5 +4,13 @@ extends GdUnitTestSuite
 @warning_ignore('unused_parameter')
 @warning_ignore('return_value_discarded')
 
+var audio: Audio
+
+func before_test():
+	audio = Audio
+
 func test_ready() -> void:
-	assert_bool(Audio.is_working).is_true()
+	assert_bool(audio.is_working).is_true()
+
+func test_ready2() -> void:
+	assert_bool(audio.is_working).is_true()
