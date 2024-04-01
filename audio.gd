fe161e2f380e2f9feb3a3d21e950763a44a7dff1
Keir <keir@sneezey.uk>
2024-04-01 13:18:54 +0100

    feat: add initial

---
+++ audio.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var is_working = false
+
+func _ready():
+	var player = load("res://AudioCoffee - Warm Autumn Day.mp3")
+	is_working = true
---
+++ test/audio_test.gd
@@ -0,0 +1,8 @@
+# GdUnit generated TestSuite
+class_name AudioTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+func test_ready() -> void:
+	Audio.is_working = true
